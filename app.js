6bd2e3c428d0b8fb81578262e29b0771f245fe4f
Asthere0n <cristiansanchezsonido@gmail.com>
2025-03-14 16:08:03 +0000

    Connected to MongoDB and setted the .env file

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import 'dotenv/config';
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+//database connection
+const dbURI = process.env.MONGODB_URI;
+
+
+mongoose.connect(dbURI, {})
+.then(() => {
+    console.log('Connected to MongoDB')
+    app.listen(3000);
+})
+.then(() => {
+  console.log('Server is running on http://localhost:3000')
+})
+.catch(err => console.error('Connection error:', err));
+
